a5312797b9e862e478ffac836217968078f5efc7
Sven Olding <sven.olding@gmx.de>
2018-10-07 16:42:09 +0200

    add model and ListManager class to return sample data set

---
+++ LinqLibrary/ListManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqLibrary
+{
+    class ListManager
+    {
+        public static List<Person> LoadSampleData()
+        {
+            List<Person> output = new List<Person>();
+
+            output.Add(new Person { LastName = "Corey", FirstName = "Tim", Birthday = Convert.ToDateTime("25.2.1970"), YearsOfExperience = 20 });
+            output.Add(new Person { LastName = "Smith", FirstName = "Joe", Birthday = Convert.ToDateTime("31.3.1970"), YearsOfExperience = 24 });
+            output.Add(new Person { LastName = "Storm", FirstName = "Sue", Birthday = Convert.ToDateTime("3.6.1974"), YearsOfExperience = 21 });
+            output.Add(new Person { LastName = "Jones", FirstName = "Sara", Birthday = Convert.ToDateTime("4.8.1964"), YearsOfExperience = 30 });
+            output.Add(new Person { LastName = "Doe", FirstName = "Jamie", Birthday = Convert.ToDateTime("23.1.1968"), YearsOfExperience = 28 });
+            output.Add(new Person { LastName = "Smith", FirstName = "Mary", Birthday = Convert.ToDateTime("13.4.1965"), YearsOfExperience = 32 });
+
+            return output;
+        }
+    }
+}
---
+++ LinqLibrary/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName}, {FirstName}";
+            }
+        }
+    }
+}

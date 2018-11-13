5330534c087c72539112354f7882b8c567d7e02c
Sven Olding <sven.olding@gmx.de>
2018-11-13 13:40:37 +0100

    test

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,17 +11,17 @@ namespace ConsoleUI
         {
             List<Person> people = ListManager.LoadSampleData();
 
-            // people = people.OrderByDescending(x => x.LastName).ThenByDescending(x => x.YearsOfExperience).ToList();
-            //people = people.Where(x => x.YearsOfExperience >= 25).OrderByDescending(x => x.YearsOfExperience).ToList();
-
-            //foreach (var person in people)
-            //{
-            //    Console.WriteLine($"{person.FullName} ({person.Birthday.ToShortDateString()}): Experience {person.YearsOfExperience} years");
-            //}
-
             int yearsOfExperience = people.Sum(x => x.YearsOfExperience);
 
             Console.WriteLine("Years of experience: " + yearsOfExperience);
+
+            people = people.OrderByDescending(x => x.LastName).ThenByDescending(x => x.YearsOfExperience).ToList();
+            people = people.Where(x => x.YearsOfExperience >= 25).OrderByDescending(x => x.YearsOfExperience).ToList();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.FullName} ({person.Birthday.ToShortDateString()}): Experience {person.YearsOfExperience} years");
+            }
 
             Console.ReadLine();
         }

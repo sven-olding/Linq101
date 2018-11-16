552c6a695ea35f91b3a0c3c5f31bb9a2d126100f
Sven Olding <sven.olding@gmx.de>
2018-11-16 15:12:50 +0100

    replace ext syntax with expression syntax

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,13 +15,23 @@ namespace ConsoleUI
 
             Console.WriteLine("Years of experience: " + yearsOfExperience);
 
-            people = people.OrderByDescending(x => x.LastName).ThenByDescending(x => x.YearsOfExperience).ToList();
-            people = people.Where(x => x.YearsOfExperience >= 25).OrderByDescending(x => x.YearsOfExperience).ToList();
+            //people = people.OrderByDescending(x => x.LastName).ThenByDescending(x => x.YearsOfExperience).ToList();
+            // people = people.Where(x => x.YearsOfExperience >= 25).OrderByDescending(x => x.YearsOfExperience).ToList();
 
-            foreach (var person in people)
+            //foreach (var person in people)
+            //{
+            //    Console.WriteLine($"{person.FullName} ({person.Birthday.ToShortDateString()}): Experience {person.YearsOfExperience} years");
+            //}
+
+            var selectedPeople = from p in people
+                     orderby p.LastName descending, p.YearsOfExperience descending
+                     where p.YearsOfExperience >= 25
+                     select p;
+            foreach (var person in selectedPeople)
             {
                 Console.WriteLine($"{person.FullName} ({person.Birthday.ToShortDateString()}): Experience {person.YearsOfExperience} years");
             }
+
 
             string stringNotEmpty = "hello extension method";
 
@@ -41,7 +51,7 @@ namespace ConsoleUI
 
             Console.WriteLine(emptyList.DefaultIfEmpty("null string").First());
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 
